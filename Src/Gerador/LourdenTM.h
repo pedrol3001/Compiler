425cfe8f8d0c3c231bc527e7e1c79c9f4b811b66
GuilhermeCgSzk <180032518@aluno.unb.br>
2022-04-30 02:40:52 -0300

    ops

--- Src/Gerador/LourdenTM.h
+++ Src/Gerador/LourdenTM.h
@@ -21,7 +21,7 @@ namespace TM {
 			int line;
 	};
 
-	struct tipoRO: public Inst {	//
+	struct tipoRO: public Inst {	// opcode r1,r2,r3
 		Reg r1,r2,r3;
 		tipoRO(std::string _nome, Reg _r1, Reg _r2, Reg _r3, int _line);
 		std::string InstToStr();	
@@ -34,14 +34,14 @@ namespace TM {
 	INST_RO(IN);	// stdin  >> r1 (ignora r2 e r3)
 	INST_RO(OUT);	// stdout << r1 (ignora r2 e r3) 
 	// Aritmeticas
-	INST_RO(ADD);	// 
-	INST_RO(SUB);
-	INST_RO(MUL);
-	INST_RO(DIV);
+	INST_RO(ADD);	// r1 = r2 + r3
+	INST_RO(SUB);	// r1 = r2 + r3
+	INST_RO(MUL);	// r1 = r2 + r3
+	INST_RO(DIV);	// r1 = r2 + r3
 	// Fim de programa
-	INST_RO(HALT);
+	INST_RO(HALT);	// Termina o programa (ignora r1,r2 e r3)
 	
-	struct tipoRM: public Inst {
+	struct tipoRM: public Inst {	//  opcode r1,offset(r2)
 		Reg r1; int offset; Reg r2;
 		tipoRM(std::string _nome, Reg _r1, int _offset, Reg _r2, int _line);
 		std::string InstToStr();		
@@ -50,13 +50,12 @@ namespace TM {
 	#define INST_RM(name) struct name: public tipoRM { \
 		name (Reg _r1, int _offset, Reg _r2, int _line): tipoRM("name",_r1,_offset,_r2,_line) {}\
 	};
-	// address = (contents of r2) + offset
 	// Load
-	INST_RM(LDC)
-	INST_RM(LDA)
-	INST_RM(LD)
+	INST_RM(LDC)	// r1 = offset (ignora r2)
+	INST_RM(LDA)	// r1 = address (r2+offset)
+	INST_RM(LD)	// r1 = carrega word endereçada por address (r2+offset)
 	// Store
-	INST_RM(ST)	// store r1 in address
+	INST_RM(ST)	// guarda r1 no endereço address (r2+offset)
 	// Jump
 	INST_RM(JEQ)	// r1==0? jump r2+offset
 	INST_RM(JNE)	// r1!=0? jump r2+offset
