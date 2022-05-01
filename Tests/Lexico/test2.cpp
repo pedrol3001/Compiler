6f52219acc11b5e62c1fb54c49bdb6c9a3dc5f61
GuilhermeCgSzk <180032518@aluno.unb.br>
2022-05-01 00:29:12 -0300

    Bug estranho

--- Tests/Lexico/test2.cpp
+++ Tests/Lexico/test2.cpp
@@ -8,15 +8,16 @@ using namespace std;
 bool test(Test& tester) {
 
 	TabSim &ts = TabSim::getInstance();
-	Lexico lexico("Resources/lexico.txt");
+	Lexico lexico("Resources/teste_comentario.txt");
 	if(lexico.error()) {
 		tester.error() << "Nao foi possivel abrir o arquivo!" << endl;
 		return false;
 	}
+	
 	tester.normal() << lexico.size() << endl;
 	
-	tester.comment() << "\nTeste sem operador >>" << endl;
-	for(Token token: lexico.container) {
+	Token token;
+	while(lexico >> token) {
 		tester.normal() << "Enum: " << token() << "\tn: " << token.id() << endl;
 		
 		// Str Arr
@@ -27,29 +28,7 @@ bool test(Test& tester) {
 			tester.normal() << "Token: " << sa->str << endl;
 		}
 		
-		if(!ts[token].has("NameAtt"))
-			tester.normal() << "Token sem atributo NameAtt" << endl;
-		else {
-			NameAtt * na = (NameAtt*)ts[token]["NameAtt"];
-			tester.normal() << "Tipo: " << na->str << endl;
-		}
-		
-		tester.comment() << "===================================" << endl;
-	}
-	
-	tester.comment() << "\nTeste do operador >>" << endl;
-	Token token;
-	while(lexico >> token) {
-		tester.normal() << "Enum: " << token() << "\tn: " << token.id() << endl;
-		
-		// Str Arr
-		if(!ts[token].has("StrAtt"))
-			tester.normal() << "Token sem atributo StrArr" << endl;
-		else {
-			StrAtt * sa = (StrAtt*)ts[token]["StrAtt"];
-			tester.normal() << "Token: " << sa->str << endl;
-		}
-		
+		// Name Att
 		if(!ts[token].has("NameAtt"))
 			tester.normal() << "Token sem atributo NameAtt" << endl;
 		else {
